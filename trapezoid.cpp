eeaadc191b548bc5a742bfed8781226e88db7a25
Wei Lin Li <wlin.li2403@gmail.com>
2021-09-27 01:05:11 -0400

    added trapezoid file

---
+++ trapezoid.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+#include "functions.h"
+
+std::string trapezoid.cpp(int w, int h)
+{
+  /*
+  int h;
+  int w;
+  std::cin >> w;
+  std::cin >> h;
+  std::cout << "Input side length" << l << std::endl;
+  */
+
+  std::string shape = "Shape:\n";
+
+  for (int i = 0; i < h; i++)
+    {
+      for (int j = 0; j < w; j++)
+	{
+	  if (j < i)
+	    {
+	      shape += " ";
+	    }
+	  else
+	    {
+	      shape += "*";
+	    }
+	}
+      w--;
+      shape += "\n";
+    }
+
+  return shape;
+
+}
